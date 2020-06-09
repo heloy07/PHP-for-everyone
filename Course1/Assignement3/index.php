e149cc74e19048cb304eb20443590e2509eadff7
Heloy <heloy.leal@hotmail.com>
2020-06-09 20:38:25 +0200

    initial commit for course1

---
+++ Course1/Assignement3/index.php
@@ -0,0 +1,25 @@
+<head>
+    <title> Heloy Estevanin </title>
+
+<head>
+
+<h1>Heloy Estevanin Request / Response</h1>
+<?php
+    echo 'The SHA256 hash of "Heloy Estevanin" is: ';
+    print hash('sha256', 'Heloy Estevanin');
+?>
+<pre>
+ASCII ART:
+
+    **       **
+    **       **
+    **       **
+    ***********
+    ***********   
+    **       **
+    **       **
+    **       **
+</pre>
+<a href='fail.php'>Click here to check the error setting</a>
+<br>
+<a href='check.php'>Click here to cause a traceback</a>
